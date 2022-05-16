73070d80b06fb5b207f8ab2abd22cba3b4aa76af
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-05-16 20:32:42 +0200

    Add test: Request to camera

---
+++ tests/Camera.test.ts
@@ -0,0 +1,52 @@
+import { Request } from '../src/Request'
+import { HttpMethod } from 'urllib'
+import { Camera } from "../src/Camera"
+
+const protocol = 'https'
+const username = 'root'
+const password = 'root'
+const cameraIP = '192.168.50.34'
+const path = 'axis-cgi/applications/list.cgi'
+const method: HttpMethod = 'GET'
+const url = `${protocol}://${cameraIP}/${path}`
+const args:Map<string, string> = new Map()
+
+const camera = new Camera('camera1', cameraIP) // Create Camera object
+const request = new Request(url, method, username, password, args); // Create Request
+
+describe('Testing if camera is reachable and can send data', () => {
+    let data:JSON;
+
+    describe('Get application list from camera', () => {
+        beforeEach(async () => {
+            data = await camera.getCameraData(request);
+        })
+    })
+
+    it('Compare with the exepected output', () => {
+        expect(data).toEqual({
+            "reply": {
+              "$": {
+                "result": "ok"
+              },
+              "application": [
+                {
+                  "$": {
+                    "Name": "vmd",
+                    "NiceName": "AXIS Video Motion Detection",
+                    "Vendor": "Axis Communications",
+                    "Version": "4.4-5",
+                    "ApplicationID": "143440",
+                    "License": "None",
+                    "Status": "Stopped",
+                    "ConfigurationPage": "local/vmd/config.html",
+                    "VendorHomePage": "http://www.axis.com",
+                    "LicenseName": "Proprietary"
+                  }
+                }
+              ]
+            }
+          }
+          )
+    })
+  });
