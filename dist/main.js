dc9047c95e4a95872d1e8a1d98d3adb1366db489
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-05-16 12:50:00 +0200

    Comments and improvements

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const urllib_1 = __importDefault(require("urllib"));
 const xml2js = require('xml2js');
+/*-----------------Just for testing----------------*/
 const protocol = 'https';
 const username = 'root';
 const password = 'root';
@@ -12,29 +13,44 @@ const cameraIP = '192.168.50.34';
 const path = 'axis-cgi/applications/list.cgi';
 const method = 'GET';
 const url = `${protocol}://${cameraIP}/${path}`;
+/*--------------------------------------------------*/
+// Class that defines request
 class Request {
-    constructor(url, args) {
+    constructor(url, method, username, password) {
         this.url = url;
-        this.args = args;
+        this.username = username;
+        this.password = password;
+        this.method = method;
     }
     getURL() {
         return this.url;
     }
+    getMethod() {
+        return this.method;
+    }
+    getUsername() {
+        return this.username;
+    }
+    getPassword() {
+        return this.password;
+    }
 }
+// Get json object from a Request sent to the camera
 async function getCameraData(req) {
     try {
         // const response: Response
         const options = {
-            method: method,
+            method: req.getMethod(),
             rejectUnauthorized: false,
             // auth: "username:password" use it if you want simple auth
-            digestAuth: `${username}:${password}`,
+            digestAuth: req.getUsername() + ':' + req.getPassword(),
             headers: {
-                //'Content-Type': 'application/xml'  use it if payload is xml
+                //'Content-Type': 'application/text'  use it if payload is text
                 //'Content-Type': 'application/json' use it if payload is json 
                 'Content-Type': 'application/xml'
             }
         };
+        // Callback function that handles response from camera
         const responseHandler = (err, data, res) => {
             if (err) {
                 console.log(err);
@@ -42,6 +58,7 @@ async function getCameraData(req) {
             console.log(res.statusCode);
             console.log(res.headers);
             console.log(data.toString());
+            //Parse xml from response and generate a json object
             xml2js.parseString(data, (err, result) => {
                 if (err) {
                     throw err;
@@ -53,6 +70,7 @@ async function getCameraData(req) {
                 console.log(json);
             });
         };
+        // Send request to the camera
         urllib_1.default.request(req.getURL(), options, responseHandler);
     }
     catch (error) {
@@ -66,5 +84,5 @@ async function getCameraData(req) {
         }
     }
 }
-const req = new Request(url, []);
+const req = new Request(url, method, username, password);
 getCameraData(req);
