d433f6159b77e0b715332e8b5b00896aeb7c2f1f
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-05-25 19:32:38 +0200

    Start to implement Task class

--- src/Task.ts
+++ src/Task.ts
@@ -1,8 +1,20 @@
 class Task{
 
-    constructor(){
-        
+    methodName:string
+    args:Array<string>
+
+    constructor(methodName:string, args:Array<string>){
+        this.methodName = methodName
+        this.args = args
     }
 
+    public getMethodName():string{
+        return this.methodName
+    }
+
+    public getArgs():Array<string>{
+        return this.args
+    }
 }
+
 export { Task }
