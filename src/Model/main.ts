7a6f1298399533bef963b24c1e2eb11bf9867622
Gaetan Lavirotte <gaetan.lavirotte@etu.univ-grenoble.fr>
2022-06-01 17:58:00 +0200

    Remove unsued lines in main

--- src/Model/main.ts
+++ src/Model/main.ts
@@ -7,35 +7,9 @@ the React Application
 
 import { DeviceManager } from './DeviceManager'
 import { Twin } from './Twin'
-import * as readline from 'readline'
 import { Server } from './server/Server'
 const cameraIP = '192.168.50.34'
 const deviceManager = new DeviceManager()
-
-// var rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// function recursiveAsyncReadLine(twinProxy:Twin) {
-//     rl.question('Do you want to switch the light? [y/n] ', (answer) => {
-//         switch(answer.toLowerCase()) {
-//             case 'y':
-//                 twinProxy.proxySwitchLight = true
-//                 break;
-//             case 'n':
-//                 console.log('Sorry! :(')
-//                 break;
-//             default:
-//                 console.log('Invalid answer!')
-//                 break;
-//         }
-//         return recursiveAsyncReadLine(twinProxy); //Calling this function again to ask new question
-//         }
-//     )
-// }
-// deviceManager.createTwin(cameraIP)
-//     .then((twinProxy:Twin) => {recursiveAsyncReadLine(twinProxy)})
 
 const server = new Server(8000)
 
